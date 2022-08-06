16866c163ccab6c7f6bb5247871c99ea420eefc1
samiyagmur <samiyagmur92@gmail.com>
2022-08-06 16:38:37 +0300

    UIButtonAdded

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -23,24 +23,25 @@ namespace Managers
 
 
         #endregion
-      
+        
         #region Event Subcription
 
         private void OnEnable()
         {
             SubscribeEvents();
+            
         }
 
         private void SubscribeEvents()
         {
-            UISignals.Instance.onOpenPanel += uiPanelController.OpenPanel;
-            UISignals.Instance.onClosePanel += uiPanelController.ClosePanel;
+            UISignals.Instance.onOpenPanel += OnOpenPanel;
+            UISignals.Instance.onClosePanel += OnClosePanel;
         }
 
         private void UnsubscribeEvents()
         {
-            UISignals.Instance.onOpenPanel -= uiPanelController.OpenPanel;
-            UISignals.Instance.onClosePanel -= uiPanelController.ClosePanel;
+            UISignals.Instance.onOpenPanel -= OnOpenPanel;
+            UISignals.Instance.onClosePanel -= OnClosePanel;
 
         }
 
@@ -51,26 +52,59 @@ namespace Managers
 
         #endregion
 
-        //#region PanelControls
-        //private void OnOpenPanel(uiPanels panels)
-        //{
-        //    uiPanelController.OpenPanel(panels);
-        //}
-        //private void OnClosePanel(uiPanels panels)
-        //{
-        //    uiPanelController.ClosePanel(panels);
-        //}
-        //#endregion
+        #region PanelControls
+        private void OnOpenPanel(uiPanels panels)
+        {
+            uiPanelController.OpenPanel(panels);
+        }
+        private void OnClosePanel(uiPanels panels)
+        {
+            uiPanelController.ClosePanel(panels);
+        }
+        #endregion
 
         public void StartButton()
         {
             UISignals.Instance.onClosePanel?.Invoke(uiPanels.startPanel);
-            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.levelPanel);
+            
+        }
+        public void OnFail()
+        {
+            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.failPanel);
+            //CoreGamesignals.Instance.onReset?.Invoke();
         }
 
+        public void OnEnterMiniGame()
+        {   
+            UISignals.Instance.onClosePanel?.Invoke(uiPanels.levelPanel);
+            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.miniGamePanel);
+            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.IdlePanel);
+        }
+        public void EnterIdleArea()
+        {   
+            UISignals.Instance.onClosePanel?.Invoke(uiPanels.miniGamePanel);
+        }
+
+        public void TryAgain()
+        {
+            UISignals.Instance.onClosePanel?.Invoke(uiPanels.failPanel);
+            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.startPanel);
+            // CoreGamesignals.Instance.onReset?.Invoke();
+        }
+
+        public void NextLevel()
+        {
+            UISignals.Instance.onClosePanel?.Invoke(uiPanels.IdlePanel);
+            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.levelPanel);
+            //CoreGamesignals.Instance.onNextLevel?.Invoke();
+        }
+        public void Restart()
+        {
+            UISignals.Instance.onOpenPanel?.Invoke(uiPanels.startPanel);
+            // CoreGamesignals.Instance.onReset?.Invoke();
+            
+        }
         
-
-
     }
 
 }
