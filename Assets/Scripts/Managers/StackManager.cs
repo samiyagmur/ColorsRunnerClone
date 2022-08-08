8298c579c423bc256b95e2b7cbf8cb976c94e04f
hakanmelen <melenglobal@gmail.com>
2022-08-08 10:02:09 +0300

    StackManager,StackSignals

--- Assets/Scripts/Managers/StackManager.cs
+++ Assets/Scripts/Managers/StackManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Signals;
 using UnityEngine;
 
 namespace Managers
@@ -20,7 +22,10 @@ namespace Managers
         #endregion
 
         #region Serialized Variables
-
+        
+        [SerializeField] private List<GameObject> cubeList = new List<GameObject>();
+        [SerializeField] [Range(0.1f, 1f)] private float lerpDelay;
+        public Transform PlayerTransform;
         
 
         #endregion
@@ -36,12 +41,14 @@ namespace Managers
 
         private void SubscribeEvents()
         {
-            
+            StackSignals.Instance.onIncreaseStack += OnAddStack;
+            StackSignals.Instance.onIncreaseStack += OnRemoveStack;
         }
 
         private void UnsubscribeEvents()
         {
-            
+            StackSignals.Instance.onIncreaseStack -= OnAddStack;
+            StackSignals.Instance.onIncreaseStack -= OnRemoveStack;
         }
         private void OnDisable()
         {
@@ -49,5 +56,55 @@ namespace Managers
         }
 
         #endregion
+        
+        private void FixedUpdate()
+        {
+            LerpStack();
+        }
+
+        private void OnAddStack(GameObject currentStack)
+        {   
+            currentStack.transform.SetParent(transform);
+
+            if (cubeList.Count == 0)
+            {
+                currentStack.transform.localPosition = transform.localPosition;
+                cubeList.Add(currentStack);
+
+                return;
+            }
+            
+            currentStack.transform.localPosition = cubeList[cubeList.Count-1].transform.localPosition + Vector3.back * 1.2f;
+            
+            cubeList.Add(currentStack);
+            
+        }
+
+        private void OnRemoveStack(GameObject currentStack)
+        {
+            
+        }
+        
+        private void LerpStack()
+        {
+            for (int i = 0; i < cubeList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Vector3 xPos = cubeList[i].transform.position;
+                    Vector3 targetPos = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y,
+                        cubeList[i].transform.position.z);
+                    cubeList[i].transform.position = Vector3.Lerp(xPos, targetPos,lerpDelay);
+                }
+                else
+                {
+                    Vector3 xPos = cubeList[i].transform.position;
+                    Vector3 targetPos = new Vector3(cubeList[i-1].transform.position.x,
+                        cubeList[i-1].transform.position.y, cubeList[i].transform.position.z);
+                    cubeList[i].transform.position = Vector3.Lerp(xPos, targetPos,lerpDelay);
+                }
+            }
+        }
+        
     }
 }
--- Assets/Scripts/Signals/StackSignals.cs
+++ Assets/Scripts/Signals/StackSignals.cs
@@ -1,11 +1,15 @@
 using Extentions;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Signals
 {
     public class StackSignals : MonoSingleton<StackSignals>
     {
         
+        public UnityAction<GameObject> onIncreaseStack = delegate(GameObject arg0) {  };
+        
+        public UnityAction<GameObject> onDecreaseStack = delegate(GameObject arg0) {  };
         protected override void Awake()
         {
             base.Awake();
