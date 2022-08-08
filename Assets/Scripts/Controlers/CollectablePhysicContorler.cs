8e6564000a9807970840b1af37d0e704b7a46f3c
samiyagmur <samiyagmur92@gmail.com>
2022-08-08 08:25:12 +0300

    collectablePhysicsController

---
+++ Assets/Scripts/Controlers/CollectablePhysicContorler.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Controlers
+{
+    public class CollectablePhysicContorler : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+
+            //if (other.CompareTag("player"))
+            //{
+                
+            //}
+
+            if (other.CompareTag("collectable"))
+            {
+                
+            }
+
+            if (other.CompareTag("obstacle"))
+            {
+
+            }
+
+            if (other.CompareTag("rainbow"))
+            {
+
+            }
+
+            if (other.CompareTag("color"))
+            {
+
+            }
+
+            if (other.CompareTag("taretArea"))
+            {
+
+            }
+
+            if (other.CompareTag("taretArea"))
+            {
+
+            }
+
+            if (other.CompareTag("dronArea"))
+            {
+
+            }
+
+            if (other.CompareTag("buildingTextArea"))
+            {
+
+            }
+            if (other.CompareTag("nextIdleLevel"))
+            {
+
+            }
+
+
+            //if (collectablescale)
+            //{
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/CollectableMenager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers
+{
+    public class CollectableMenager : MonoBehaviour
+    {
+
+    }
+}
