c54ec85f079b918c5bc1e81188979d87560b82e2
hakanmelen <melenglobal@gmail.com>
2022-08-10 20:56:56 +0300

    stack updated

--- Assets/Scripts/Managers/StackManager.cs
+++ Assets/Scripts/Managers/StackManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Signals;
 using UnityEngine;
 
@@ -59,7 +60,8 @@ namespace Managers
         
         private void FixedUpdate()
         {
-            LerpStack();
+           LerpStackWithMathf();
+           //LerpStackWithVector3();
         }
 
         private void OnAddStack(GameObject currentStack)
@@ -92,26 +94,56 @@ namespace Managers
             }
         }
         
-        private void LerpStack()
+        private void LerpStackWithVector3()
         {
             for (int i = 0; i < collectableList.Count; i++)
-            {
+            {   
+                Debug.Log(collectableList.Count);
                 if (i == 0)
                 {
-                    Vector3 xPos = collectableList[i].transform.position;
-                    Vector3 targetPos = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y,
-                        collectableList[i].transform.position.z);
-                    collectableList[i].transform.position = Vector3.Lerp(xPos, targetPos,lerpDelay);
+                    var  collectablePos = collectableList.ElementAt(i);
+                    Vector3 targetPos = PlayerTransform.localPosition;
+                    collectablePos .transform.position= Vector3.Lerp(collectablePos.transform.position, targetPos,lerpDelay);
                 }
                 else
                 {
-                    Vector3 xPos = collectableList[i].transform.position;
-                    Vector3 targetPos = new Vector3(collectableList[i-1].transform.position.x,
-                        collectableList[i-1].transform.position.y, collectableList[i].transform.position.z);
-                    collectableList[i].transform.position = Vector3.Lerp(xPos, targetPos,lerpDelay);
+                    var collectablePos = collectableList.ElementAt(i);
+                    var targetPos = collectableList.ElementAt(i-1);
+                    collectablePos.transform.position = Vector3.Lerp(collectablePos.transform.position, targetPos.transform.position,lerpDelay);
+                   
+                }
+                
+            }
+        }
+
+        private void LerpStackWithMathf()
+        {
+            for (int i = 0; i < collectableList.Count; i++)
+            {   
+                Debug.Log(collectableList.Count);
+                
+                if (i == 0)
+                {
+                    var collectablePos = collectableList.ElementAt(i);
+                    Vector3 targetPos = PlayerTransform.position;
+                    
+                    targetPos = new Vector3(
+                        Mathf.Lerp(targetPos.x, collectablePos.transform.position.x, 0.7f),
+                        Mathf.Lerp(targetPos.y, collectablePos.transform.position.y, 0.7f),
+                        Mathf.Lerp(targetPos.z, collectablePos.transform.position.z - 1.5f, 0.7f));
+                }
+                else
+                {
+                    var collectablePos = collectableList.ElementAt(i-1);
+                    var targetPos = collectableList.ElementAt(i);
+                    targetPos.transform.position = new Vector3(
+                        Mathf.Lerp(targetPos.transform.position.x, collectablePos.transform.position.x, 10*Time.fixedDeltaTime),
+                        Mathf.Lerp(targetPos.transform.position.y, collectablePos.transform.position.y, 10*Time.fixedDeltaTime),
+                        Mathf.Lerp(targetPos.transform.position.z, collectablePos.transform.position.z - 1.5f, 10*Time.fixedDeltaTime));
                 }
             }
         }
+        
         
     }
 }
