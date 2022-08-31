a172bf7770f21c56a703cd39f90bcfdb538f375c
Muhammet Yasin YILDIZ <mysnyldz@gmail.com>
2022-09-01 00:30:20 +0300

    Fixed State-Machine

--- Assets/Scripts/Controllers/StickmanWalkingState.cs
+++ Assets/Scripts/Controllers/StickmanWalkingState.cs
@@ -1,5 +1,6 @@
 ﻿using Abstract;
 using System.Collections;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,27 +11,42 @@ namespace Controllers
         private readonly StickManAIController _stickManAIController;
         private Animator _animator;
         private NavMeshAgent _navMeshAgent;
+        private Vector3 _lastPost=Vector3.zero;
 
-        public StickmanWalkingState(StickManAIController stickManAIController, Animator animator, NavMeshAgent navMeshAgent)
+        public float WaitingTime;
+
+        public StickmanWalkingState(StickManAIController stickManAIController, Animator animator,
+            NavMeshAgent navMeshAgent)
         {
             _stickManAIController = stickManAIController;
             _animator = animator;
             _navMeshAgent = navMeshAgent;
         }
 
+        public void OnSetup()
+        {
+            if (Vector3.Distance(_stickManAIController.Target.transform.position,_lastPost)<=0f)
+            {
+                WaitingTime += Time.deltaTime;
+            }
+            
+            _lastPost = _stickManAIController.Target.transform.position;
+            
+        }
+
         public void OnEnter()
         {
-            
+            WaitingTime = 0f;
+            _navMeshAgent.enabled = true;
+            _navMeshAgent.SetDestination(_stickManAIController.Target.transform.position);
+            //_animator.SetFloat("Speed", 1f);
         }
 
         public void OnExit()
         {
+            _navMeshAgent.enabled = false;
+            //_animator.SetFloat("Speed",0f);
             
-        }
-
-        public void OnSetup()
-        {
-           
         }
     }
 }
